2a734d480e282c7773a7c78d748bdcca21c5203c
Callum <callum.king.underwood@googlemail.com>
2020-09-19 18:30:09 +0100

    Initial Commit
    Initial commit

---
+++ Urmel/UrmelDesktop/Program.cs
@@ -0,0 +1,15 @@
+﻿using Urmel;
+using System;
+
+namespace UrmelDesktop
+{
+  public static class Program
+  {
+    [STAThread]
+    static void Main()
+    {
+      using var game = new Game1();
+      game.Run();
+    }
+  }
+}
---
+++ Urmel/UrmelTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace UrmelTests
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+
+    }
+  }
+}
