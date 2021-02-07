c991f2740d7c749202c8b27c09e2a924a9d0bf82
regroove23 <45squire@gmail.com>
2021-02-07 00:37:26 +0000

    Transform stuff
    cleaned up transform code, added summaries, completed unit tests for transform

--- Urmel/BCEngine/Math/Transform.cs
+++ Urmel/BCEngine/Math/Transform.cs
@@ -20,6 +20,13 @@ namespace BCEngine.Math
         Rotation = MathHelper.ToRadians(value);
       }
     }
+
+    /// <summary>
+    /// Creates a world space transform from a parent transform and child transform where the child coordinates are localised to the parent
+    /// </summary>
+    /// <param name="parent">Composition parent</param>
+    /// <param name="child">Composition child</param>
+    /// <returns>transform result</returns>
     public static Transform Compose(Transform parent, Transform child)
     {
       Transform result = new Transform();
@@ -29,35 +36,60 @@ namespace BCEngine.Math
       return result;
     }
 
-    public static void Lerp(ref Transform from, ref Transform to, float amount, ref Transform result)
+    /// <summary>
+    /// Linear interpolation between two transforms by a float amount, usually from 0 to 1
+    /// </summary>
+    /// <param name="from">The transform to interpolate from</param>
+    /// <param name="to">The transform to interpolate to</param>
+    /// <param name="amount">The amount of interpolation</param>
+    /// <returns>transform result</returns>
+    public static Transform Lerp(Transform from, Transform to, float amount)
     {
+      Transform result = new Transform();
       result.Position = Vector2.Lerp(from.Position, to.Position, amount);
       result.Scale = Vector2.Lerp(from.Scale, to.Scale, amount);
       result.Rotation = MathHelper.Lerp(from.Rotation, to.Rotation, amount);
+      return result;
     }
-
-    public Vector2 TransformVector(Vector2 point)
+    /// <summary>
+    /// Transforms a Vector2 from local space to world space. The opposite of Transform.InverseTransformVector
+    /// </summary>
+    /// <param name="point">local space Vector2</param>
+    /// <param name="transform">transform that point is localised to</param>
+    /// <returns>localised Vector2 position</returns>
+    public static Vector2 TransformVector(Transform transform, Vector2 point)
     {
-      Vector2 result = Vector2.Transform(point, Matrix.CreateRotationZ(Rotation));
-      result *= Scale;
-      result += Position;
+      Vector2 result = Vector2.Transform(point, Matrix.CreateRotationZ(transform.Rotation));
+      result *= transform.Scale;
+      result += transform.Position;
+      return result;
+    }
+    /// <summary>
+    /// Transforms the vector x, y from world space to local space. The opposite of Transform.TransformVector
+    /// </summary>
+    /// <param name="point">world space vector2</param>
+    /// <param name="transform">transform to localise point into</param>
+    /// <returns>world space Vector2</returns>
+    public static Vector2 InverseTransformVector(Transform transform, Vector2 point)
+    {
+      Vector2 result = point - transform.Position;
+      result = Vector2.Transform(result, Matrix.CreateRotationZ(-transform.Rotation));
+      result /= transform.Scale;
       return result;
     }
 
-    public Vector2 InverseTransformVector(Vector2 point)
-    {
-      Vector2 result = point - Position;
-      result = Vector2.Transform(result, Matrix.CreateRotationZ(-Rotation));
-      result /= Scale;
-      return result;
-    }
-
-    public Vector2 GetRelativePosition(Vector2 RelativeTo)
+    /// <summary>
+    /// Gets the relative position from a transform relative to a world space vector2
+    /// </summary>
+    /// <param name="RelativeTo">the reference point to measure the relative distance from</param>
+    /// <param name="transform">the transform to measure to</param>
+    /// <returns>relative position Vector2</returns>
+    public static Vector2 GetRelativePosition(Transform transform, Vector2 RelativeTo)
     {
       Transform result = new Transform();
-      result.Position = RelativeTo * -Position;
-      result.Rotation = -Rotation;
-      result.Scale = -Scale;
+      result.Position = RelativeTo * -transform.Position;
+      result.Rotation = -transform.Rotation;
+      result.Scale = -transform.Scale;
       return result.Position;
     }
   }
--- Urmel/BCEngineTests/Math/TransformTests.cs
+++ Urmel/BCEngineTests/Math/TransformTests.cs
@@ -1,56 +1,52 @@
 ﻿using BCEngine.Math;
 using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Xunit;
 
 namespace BCEngineTests.Math
 {
   public class TransformTests
   {
-    /// <summary>
-    /// Temporarily Commented out to preserve unit tests for a time when the code can be reused with GameObject implementations
-    /// </summary>
-
-    /*
-    [Fact]
-    public void TransformConstructorDoesntCrash()
-    {
-      Transform transform = new Transform();
-      Assert.NotNull(transform);
-    }
-
-    [Fact]
-    public void CanAddChildTransformToTransform()
-    {
-      Transform transform1 = new Transform();
-      Transform transform2 = new Transform();
-      transform1.AddTransform(transform2);
-
-      Assert.Contains(transform2, transform1.GetChildren());
-      Assert.Equal(transform1, transform2.Parent);
-    }
-    
-    [Fact]
-    public void CanRemoveChildTransformFromTransform()
-    {
-      Transform transform1 = new Transform();
-      Transform transform2 = new Transform();
-      transform1.AddTransform(transform2);
-
-      Assert.Contains(transform2, transform1.GetChildren());
-      Assert.Equal(transform1, transform2.Parent);
-
-      transform1.RemoveTransform(transform2);
-      Assert.DoesNotContain(transform2, transform1.GetChildren());
-      Assert.NotEqual(transform1, transform2.Parent);
-    }
-    */
     [Theory]
     [MemberData(nameof(ComposeTransformsData))]
-    public void CanComposeTransformPositions(Transform parent, Transform child, Transform expectedResult)
+    public void CanComposeTransforms(Transform parent, Transform child, Transform expectedResult)
     {
       Transform composeResult = Transform.Compose(parent, child);
       Assert.Equal(expectedResult, composeResult);
+    }
+
+    [Theory]
+    [MemberData(nameof(LerpTransformsData))]
+    public void CanLerpTransforms(Transform from, Transform to, float amount, Transform expectedResult)
+    {
+      Transform lerpResult = Transform.Lerp(from, to, amount);
+      Assert.Equal(expectedResult, lerpResult);
+    }
+
+    [Theory]
+    [MemberData(nameof(TransformVectorData))]
+    public void CanTransformVector(Transform transform, Vector2 vector, Vector2 expectedResult)
+    {
+      Vector2 TransformVectorResult = Transform.TransformVector(transform, vector);
+      Assert.Equal(TransformVectorResult, expectedResult);
+    }
+
+    [Theory]
+    [MemberData(nameof(InverseTransformVectorData))]
+    public void CanInverseTransformVector(Transform transform, Vector2 vector, Vector2 expectedResult)
+    {
+      Vector2 InverseTransformVectorResult = Transform.InverseTransformVector(transform, vector);
+      Assert.Equal(InverseTransformVectorResult, expectedResult);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetRelativePositionData))]
+    public void CanGetRelativePosition(Transform transform, Vector2 vector, Vector2 expectedResult)
+    {
+      Vector2 GetRelativePositionResult = Transform.GetRelativePosition(transform, vector);
+      Assert.Equal(GetRelativePositionResult, expectedResult);
     }
 
     public static IEnumerable<object[]> ComposeTransformsData()
@@ -137,6 +133,246 @@ namespace BCEngineTests.Math
         new object[]{ parent2, child2, expected2 },
         new object[]{ parent3, child3, expected3 },
         new object[]{ parent4, child4, expected4 }
+      };
+
+      return allData;
+    }
+
+    public static IEnumerable<object[]> LerpTransformsData()
+    {
+      Transform from1 = new Transform()
+      {
+        Position = new Vector2(10.85f, 99.4f),
+        Rotation = 5.4f,
+        Scale = Vector2.One
+      };
+      Transform to1 = new Transform()
+      {
+        Position = new Vector2(15f, 12f),
+        Rotation = 0.1f,
+        Scale = new Vector2(5, 1)
+      };
+      float amount1 = 0.5f;
+      Transform expected1 = new Transform()
+      {
+        Position = new Vector2(12.925f, 55.7f),
+        Rotation = 2.75f,
+        Scale = new Vector2(3f, 1f)
+      };
+
+      Transform from2 = new Transform()
+      {
+        Position = new Vector2(15f, 4f),
+        Rotation = 0.4f,
+        Scale = new Vector2(114, 22)
+      };
+      Transform to2 = new Transform()
+      {
+        Position = new Vector2(1f, 120f),
+        Rotation = 00f,
+        Scale = new Vector2(445, 12)
+      };
+      float amount2 = 0.75f;
+      Transform expected2 = new Transform()
+      {
+        Position = new Vector2(4.5f, 91f),
+        Rotation = 0.099999994f,
+        Scale = new Vector2(362.25f, 14.5f)
+      };
+
+      Transform from3 = new Transform()
+      {
+        Position = new Vector2(1f, 43f),
+        Rotation = -90f,
+        Scale = new Vector2(14, 220)
+      };
+      Transform to3 = new Transform()
+      {
+        Position = new Vector2(-231f, 120f),
+        Rotation = 90f,
+        Scale = new Vector2(405, 12)
+      };
+      float amount3 = 5.5f;
+      Transform expected3 = new Transform()
+      {
+        Position = new Vector2(-1275f, 466.5f),
+        Rotation = 900f,
+        Scale = new Vector2(2164.5f, -924f)
+      };
+
+      Transform from4 = new Transform()
+      {
+        Position = new Vector2(-10f, -43f),
+        Rotation = -50f,
+        Scale = new Vector2(1, 1)
+      };
+      Transform to4 = new Transform()
+      {
+        Position = new Vector2(21f, -20f),
+        Rotation = -12f,
+        Scale = new Vector2(405, 12)
+      };
+      float amount4 = 0f;
+      Transform expected4 = new Transform()
+      {
+        Position = new Vector2(-10f, -43f),
+        Rotation = -50f,
+        Scale = new Vector2(1f, 1f)
+      };
+
+      var allData = new List<object[]>
+      {
+        new object[]{ from1, to1, amount1, expected1 },
+        new object[]{ from2, to2, amount2, expected2 },
+        new object[]{ from3, to3, amount3, expected3 },
+        new object[]{ from4, to4, amount4, expected4 }
+      };
+
+      return allData;
+    }
+
+    public static IEnumerable<object[]> TransformVectorData()
+    {
+      Transform transform1 = new Transform()
+      {
+        Position = new Vector2(10.85f, 99.4f),
+        Rotation = 5.4f,
+        Scale = Vector2.One
+      };
+      Vector2 vector1 = new Vector2(15f, 12f);
+      Vector2 expected1 = new Vector2(29.643568f, 95.42485f);
+
+      Transform transform2 = new Transform()
+      {
+        Position = new Vector2(15f, 4f),
+        Rotation = 0.4f,
+        Scale = new Vector2(114, 22)
+      };
+      Vector2 vector2 = new Vector2(1f, 120f);
+      Vector2 expected2 = new Vector2(-5207.2417f, 2444.1682f);
+
+      Transform transform3 = new Transform()
+      {
+        Position = new Vector2(1f, 43f),
+        Rotation = -90f,
+        Scale = new Vector2(14, 220)
+      };
+      Vector2 vector3 = new Vector2(-231f, 120f);
+      Vector2 expected3 = new Vector2(2951.9844f, 33646.766f);
+
+      Transform transform4 = new Transform()
+      {
+        Position = new Vector2(-10f, -43f),
+        Rotation = -50f,
+        Scale = new Vector2(1, 1)
+      };
+      Vector2 vector4 = new Vector2(21f, -20f);
+      Vector2 expected4 = new Vector2(15.511784f, -56.789448f);
+
+      var allData = new List<object[]>
+      {
+        new object[]{ transform1, vector1, expected1 },
+        new object[]{ transform2, vector2, expected2 },
+        new object[]{ transform3, vector3, expected3 },
+        new object[]{ transform4, vector4, expected4 }
+      };
+
+      return allData;
+    }
+
+    public static IEnumerable<object[]> InverseTransformVectorData()
+    {
+      Transform transform1 = new Transform()
+      {
+        Position = new Vector2(10.85f, 99.4f),
+        Rotation = 5.4f,
+        Scale = Vector2.One
+      };
+      Vector2 vector1 = new Vector2(15f, 12f);
+      Vector2 expected1 = new Vector2(70.173584f, -52.265198f);
+
+      Transform transform2 = new Transform()
+      {
+        Position = new Vector2(15f, 4f),
+        Rotation = 0.4f,
+        Scale = new Vector2(114, 22)
+      };
+      Vector2 vector2 = new Vector2(1f, 120f);
+      Vector2 expected2 = new Vector2(0.28313747f, 5.104315f);
+
+      Transform transform3 = new Transform()
+      {
+        Position = new Vector2(1f, 43f),
+        Rotation = -90f,
+        Scale = new Vector2(14, 220)
+      };
+      Vector2 vector3 = new Vector2(-231f, 120f);
+      Vector2 expected3 = new Vector2(2.508238f, -1.0995859f);
+
+      Transform transform4 = new Transform()
+      {
+        Position = new Vector2(-10f, -43f),
+        Rotation = -50f,
+        Scale = new Vector2(1, 1)
+      };
+      Vector2 vector4 = new Vector2(21f, -20f);
+      Vector2 expected4 = new Vector2(35.948566f, 14.060597f);
+
+      var allData = new List<object[]>
+      {
+        new object[]{ transform1, vector1, expected1 },
+        new object[]{ transform2, vector2, expected2 },
+        new object[]{ transform3, vector3, expected3 },
+        new object[]{ transform4, vector4, expected4 }
+      };
+
+      return allData;
+    }
+
+    public static IEnumerable<object[]> GetRelativePositionData()
+    {
+      Transform transform1 = new Transform()
+      {
+        Position = new Vector2(10.85f, 99.4f),
+        Rotation = 5.4f,
+        Scale = Vector2.One
+      };
+      Vector2 vector1 = new Vector2(15f, 12f);
+      Vector2 expected1 = new Vector2(-162.75f, -1192.8f);
+
+      Transform transform2 = new Transform()
+      {
+        Position = new Vector2(15f, 4f),
+        Rotation = 0.4f,
+        Scale = new Vector2(114, 22)
+      };
+      Vector2 vector2 = new Vector2(1f, 120f);
+      Vector2 expected2 = new Vector2(-15f, -480f);
+
+      Transform transform3 = new Transform()
+      {
+        Position = new Vector2(1f, 43f),
+        Rotation = -90f,
+        Scale = new Vector2(14, 220)
+      };
+      Vector2 vector3 = new Vector2(-231f, 120f);
+      Vector2 expected3 = new Vector2(231f, -5160f);
+
+      Transform transform4 = new Transform()
+      {
+        Position = new Vector2(-10f, -43f),
+        Rotation = -50f,
+        Scale = new Vector2(1, 1)
+      };
+      Vector2 vector4 = new Vector2(21f, -20f);
+      Vector2 expected4 = new Vector2(210f, -860f);
+
+      var allData = new List<object[]>
+      {
+        new object[]{ transform1, vector1, expected1 },
+        new object[]{ transform2, vector2, expected2 },
+        new object[]{ transform3, vector3, expected3 },
+        new object[]{ transform4, vector4, expected4 }
       };
 
       return allData;
